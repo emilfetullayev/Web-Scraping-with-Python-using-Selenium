0da053978c870e335abfef5f4597b3b76ce5937b
Emil Fetullayev <69794670+emilfetullayev@users.noreply.github.com>
2021-11-09 19:36:08 +0400

    first commit

---
+++ linkedin.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+
+s = Service("C:\Program Files (x86)\chromedriver.exe")
+
+browser = webdriver.Chrome(service=s)
+
+browser.get("https://www.linkedin.com/")
+
+
+time.sleep(5)
+
+login = browser.find_element_by_xpath("/html/body/nav/div/a[2]")
+login.click()
+time.sleep(5)
+
+email = browser.find_element_by_xpath("//*[@id='username']")
+password = browser.find_element_by_xpath("//*[@id='password']")
+email.send_keys("emil.fetullayev.1992@gmail.com")
+password.send_keys("emil456654")
+
+login_button = browser.find_element_by_css_selector("#organic-div > form > div.login__form_action_container > button")
+login_button.click()
+time.sleep(5)
+
+search_bar =browser.find_element_by_xpath("//*[@id='global-nav-typeahead']/input")
+search_bar.send_keys("#python")
+search_bar.send_keys(Keys.RETURN)
+time.sleep(10)
+
+contacts = browser.find_element_by_xpath("//*[@id='ember20']/span")
+contacts.click()
+time.sleep(5)
+
+contact_button = browser.find_element_by_class_name("mn-community-summary__entity-info")
+contact_button.click()
+time.sleep(5)
+
+
+for i in range(1,3):
+    browser.execute_script('window.scrollTo(0,document.body.scrollHeight)')
+    time.sleep(3)
+    
+followers = browser.find_elements_by_class_name("mn-connection-card__details")
+
+fallowerList=[]
+
+for follower in followers:
+    fallowerList.append(follower.text)
+  
+
+with open ("follower.txt","w",encoding = "UTF-8") as file:
+    for follower in fallowerList:
+        file.write(follower + "/n")
+time.sleep(5)
+
+browser.quit()
+
+
+
+
+
+
+
+
+
+
